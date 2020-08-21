428e164a3d00adb26816bf66c7f57067f4cff6f0
Arjun-810 <arjunkaushik810@gmail.com>
2020-08-21 20:15:36 +0530

    sixth commit

--- backend/student_portal/student_portal/settings.py
+++ backend/student_portal/student_portal/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
 'ENGINE': 'django.db.backends.mysql',
 'NAME': 'Student',
 'USER':'root',
+'PASSWORD':'',
 'HOST': '127.0.0.1',
 'PORT': '3306',
 'OPTIONS': {
---
+++ frontend/url.js
@@ -0,0 +1,2 @@
+var api = 'https://dce90745f4d1.ngrok.io';
+localStorage.setItem('api',api);
